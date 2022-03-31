8b4568741d34024ff0f5caf97166a4bc75cfaaa2
carooobregon <carooobregon@gmail.com>
2022-03-31 17:42:25 -0600

    env working

--- myApp/src/views/HomePage.vue
+++ myApp/src/views/HomePage.vue
@@ -2,21 +2,13 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
     
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <p> Logged In </p>
+      <ion-button> LOGOUT </ion-button>  
     </ion-content>
   </ion-page>
 </template>
