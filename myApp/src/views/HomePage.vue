77d7e3d2e6bc1eef5bf1aef57a6f85bb93319770
carooobregon <carooobregon@gmail.com>
2022-05-02 11:33:49 -0500

    adding projects tab

--- myApp/src/views/HomePage.vue
+++ myApp/src/views/HomePage.vue
@@ -357,13 +357,37 @@
                   <p class="text-sm font-medium text-gray-500">
                     {{ chats.length }} chat(s)
                   </p>
+                  <Menu as="div" class="relative inline-block text-left">
+                    <div>
+                      <MenuButton class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                        Projects
+                        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                      </MenuButton>
+                    </div>
+                    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                      <MenuItems class="z-[1] origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div class="py-1">
+                          <MenuItem v-slot="{ active }">
+                            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Account settings</a>
+                          </MenuItem>
+                          <MenuItem v-slot="{ active }">
+                            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Support</a>
+                          </MenuItem>
+                          <MenuItem v-slot="{ active }">
+                            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">License</a>
+                          </MenuItem>
+                          <form method="POST" action="#">
+                            <MenuItem v-slot="{ active }">
+                              <button type="submit" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full text-left px-4 py-2 text-sm']">Sign out</button>
+                            </MenuItem>
+                          </form>
+                        </div>
+                      </MenuItems>
+                    </transition>
+                  </Menu>
                 </div>
               </div>
-              <div
-                class="border-t border-b border-gray-200 bg-gray-50 px-6 py-2 text-sm font-medium text-gray-500"
-              >
-                Sorted by date
-              </div>
+              
             </div>
             <nav aria-label="Chat list" class="min-h-0 flex-1 overflow-y-auto">
               <ul
