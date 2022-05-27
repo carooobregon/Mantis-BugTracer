0cbec1feee775e6ba5f397bcd49fb9aa3e8b3263
Diego Frias <diego9816@hotmail.com>
2022-05-26 22:04:45 -0500

    IN-65 Crear nuevo chat al escanear QR (#8)
    * IN-65 Crear nuevo chat al escanear QR
    
    * IN-65 Crear nuevo chat al escanear QR

--- myApp/src/views/CreateNewChatPage.vue
+++ myApp/src/views/CreateNewChatPage.vue
@@ -6,8 +6,21 @@
 
 <script>
 import SyncLoader from "vue-spinner/src/SyncLoader.vue";
+import { Auth } from "aws-amplify";
 
 export default {
+  created() {
+    const project_id = this.$route.params.project_id;
+    Auth.currentAuthenticatedUser()
+      .then(() => {
+        // SIGNED IN -> create new chat
+      })
+      .catch(() => {
+        localStorage.setItem("project_id", project_id);
+        localStorage.setItem("pending_new_chat", true);
+        this.$router.push({ name: "Auth" });
+      });
+  },
   components: {
     SyncLoader,
   },
